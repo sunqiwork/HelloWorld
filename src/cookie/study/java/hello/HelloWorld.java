8648ceeaa7d93db3f88008a9b05b63985e44998c
Cookie <cookie@Cookies-mini-Macbook.local>
2017-05-05 19:06:33 +0800

    Initial commit

---
+++ src/cookie/study/java/hello/HelloWorld.java
@@ -0,0 +1,10 @@
+package cookie.study.java.hello;
+
+/**
+ * Created by cookie on 2017/5/5.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World!!!");
+    }
+}
